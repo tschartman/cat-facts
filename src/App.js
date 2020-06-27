e454ca802689b5d63ba1a8e9cd78d0c4cb046971
tim <tschartman@yahoo.com>
2020-06-26 20:58:29 -0500

    init repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import NestedList from './components/NestedList';
+import { Grid, Button } from '@material-ui/core';
 
-function App() {
+export default function App() {
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    async function fetchApi() {
+      const response = await axios.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=40');
+      setList(response.data);
+    }
+    fetchApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Cat Facts</h1>
+          <Grid container justify = "center">
+            <NestedList items={list}></NestedList>
+          </Grid>
+       </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/NestedList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function NestedList(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  return (
+      <div>
+        <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Nested List Items
+              </ListSubheader>
+            }
+            className={classes.root}
+        >
+        { props.items.map(item => {
+          return (
+            <ListItem button>
+              <ListItemIcon>
+                <SendIcon />
+              </ListItemIcon>
+              <ListItemText primary={item.text} secondary={item.createdAt} />
+            </ListItem>
+          )
+        })}
+      </List>
+    </div>
+  );
+}
